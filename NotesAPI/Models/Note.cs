99637d7404369f3870e9ac902818f87fe430a459
Nil <nilyos01@easv365.dk>
2024-09-18 15:27:07 +0200

    Revert "First commit"
    This reverts commit c8346f64e5319171981d2a6160a6af1c4a9c14ad.

--- NotesAPI/Models/Note.cs
+++
@@ -1,9 +0,0 @@
-namespace NotesAPI.Models;
-
-public class Note
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Content { get; set; } = string.Empty;
-    public DateTime CreatedOn { get; set; } = DateTime.Now;
-}
